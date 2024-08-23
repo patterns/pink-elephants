93ab83f89cdd8a63d4d40df9263b903a22ebe02b
興怡 <>
2024-08-23 05:37:01 -0700

    script step for abduco

--- nix/start-spin.sh
+++ nix/start-spin.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 
+# enter terminal mux
+##abduco -c session1 dvtm
+##cloudflared tunnel --config $HOME/.cloudflared/config.yml run $CF_TUNNEL_ID 
+
 # env var to workaround mem allocation error
 export SPIN_WASMTIME_TOTAL_MEMORIES=50
 
